0a7804155f2a047a1bc5f7dd44ecfd2e20352585
jeprinz <jacobeliasprinz@gmail.com>
2020-01-20 19:30:56 -0500

    AEx

--- Lambda.hs
+++ Lambda.hs
@@ -11,7 +11,10 @@ import Control.Monad.State
 data Exp = Var Id | App Exp Exp | Lam Id Exp | LAtomic Type String | Let Id Exp Exp deriving(Show)
 
 data AExp =
-    AVar Type Id | AApp Type AExp AExp | ALam Type Id AExp | ALAtomic Type String | ALet Type Id Exp Exp deriving(Show)
+    AVar Type Id | AApp Type AExp AExp | ALam Type Id AExp |
+    ALAtomic Type String | ALet Type Type Id AExp AExp deriving(Show)
+    -- In ALet, the first Type is the type of the Variable, the second is of the whole expression. E.g,
+    -- (let x : A = e1 in e2) : B is encoded (ALet A B x e1 e2)
 
 type TypeState' = TypeState Id Char
 
@@ -20,16 +23,27 @@ aExpToStringI (AApp t e1 e2) = do
     left <- aExpToStringI e1
     right <- aExpToStringI e2
     ts <- typeToStringI t False
-    return (show left ++ show right ++ " : " ++ ts)
+    return ("(" ++ left ++ right ++ " )[" ++ ts ++ "]")
 aExpToStringI (ALam t x e) = do
     es <- aExpToStringI e
     varName <- getName x
     ts <- typeToStringI t False
-    return ("l " ++ [varName] ++ ". " ++ es ++ " : " ++ ts)
+    return ("(l " ++ [varName] ++ ". " ++ es ++ " )[" ++ ts  ++ "]")
 aExpToStringI (AVar t x) = do
     varName <- getName x
-    return [varName]
--- aExpToStringI (ALAtomic )
+    ts <- typeToStringI t False
+    return $ "( " ++ [varName] ++ " )[" ++ ts ++ "]"
+aExpToStringI (ALAtomic t text) = do
+    ts <- typeToStringI t False
+    return $ "( " ++ text ++ " )[" ++ ts ++ "]"
+aExpToStringI (ALet tx t x e1 e2) = do -- TODO: how can it get a type for the variable x?
+    varName <- getName x
+    s1 <- aExpToStringI e1
+    s2 <- aExpToStringI e2
+    ts <- typeToStringI t False
+    txs <- typeToStringI tx False
+    return $ "(let " ++ [varName] ++ " [" ++ txs ++  "] = " ++ s1 ++ " in " ++ s2 ++ ")[" ++ ts ++ "]"
+    
 
 
 aExpToString :: AExp -> String
